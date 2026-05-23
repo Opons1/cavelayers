e0d91b2a4fadf623aaea88af2a44b388c694ae5d
Opons1 <tazimc1001@gmail.com>
2026-05-23 13:04:42 -0700

    vine

--- plants.lua
+++ plants.lua
@@ -21,5 +21,40 @@ local function register_moss(name, desc, tiles)
         sounds = default.node_sound_dirt_defaults(),
         tiles = tiles,
     })
+    core.register_craft({
+        type = "shapeless",
+        output = "cavelayers:"..name.."_carpet 8",
+        recipe = {"cavelayers:"..name},
+    })
 end
-register_moss("cavelayermoss1", "Cave Moss 1", {"cavelayerjunglemossblock.png"})
+local function register_vine(name, desc, tiles, selectionbox)
+    if not selectionbox then
+        selectionbox = {
+            type = "fixed",
+            fixed = {
+                {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5}
+            }
+        }
+    end
+    core.register_node("cavelayers:"..name, {
+        inventory_image = tiles[1],
+        description = desc,
+        drawtype = "plantlike",
+        paramtype = "light",
+        selection_box = selectionbox,
+        groups = {snappy = 3},
+        sounds = default.node_sound_leaves_defaults(),
+        tiles = tiles,
+        walkable = false,
+        climbable = true,
+        
+    })
+end
+register_moss("jungle_moss", "Jungle Moss", {"cavelayersjunglemoss.png"})
+register_moss("lush_moss", "Lush Moss", {"cavelayerslushmoss.png"})
+register_vine("lush_vine", "Lush Vine", {"cavelayerslushvine.png"}, {
+	type = "fixed",
+	fixed = {
+		{-0.2500, -0.5000, -0.2500, 0.2500, 0.5000, 0.2500}
+	}
+})
